d68f64e2190625a67364f19e1f3194c03e4c0d73
Laura Daian <laura.daian@students.unibe.ch>
2013-11-26 17:27:10 +0100

    I've added some more stuff. The recording should work now (it was a wrong reference to a page that wasn't working). I've also designed the record page to look like the other ones.

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 	<?php
 		echo "<a href=\"/index.html\"><p id=\"name\">" . $pageName . "</p></a>";
 		if (strlen($_COOKIE["username"])>1) {
-			echo "<a href=\"/useraccount.html\"><p id=\"useraccount\">User Account</p></a>";
+			echo "<a href=\"/useraccount.html\"><p id=\"useraccount\">User Account ".$_COOKIE["username"]."</p></a>";
 		} else {
 			echo "<a href=\"/login.html\"><p id=\"useraccount\">Log In</p></a>";
 		}
